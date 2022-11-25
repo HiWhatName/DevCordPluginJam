2f07b46706ee67692a634fbc0235c952e479bef2
HiWhatName <furo@localhost>
2022-11-25 20:53:07 +0100

    Init project

---
+++ src/main/java/com/pluginjam/Pluginjam.java
@@ -0,0 +1,17 @@
+package com.pluginjam;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Pluginjam extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
