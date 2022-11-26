4646cd757ce592ecf6454575b19339f4a62d527d
RoffelFoffel <moritz.brunne@gmail.com>
2022-11-26 21:51:43 +0100

    refactor JoinListener

--- src/main/java/com/pluginjam/listener/JoinListener.java
+++ src/main/java/com/pluginjam/listener/JoinListener.java
@@ -1,5 +1,6 @@
 package com.pluginjam.listener;
 
+import com.pluginjam.dungeon.generator.world.DungeonWorld;
 import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -9,17 +10,22 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 public class JoinListener implements Listener {
 
-    World Dungeon = Bukkit.getWorld("dungeon");
-    Location SpawnPoint = new Location(Dungeon, 0,75,0);
+    private final DungeonWorld dungeonWorld;
+
+    public JoinListener(DungeonWorld dungeonWorld) {
+        this.dungeonWorld = dungeonWorld;
+    }
+
     @EventHandler(priority = EventPriority.HIGH)
     public void onPlayerJoin(PlayerJoinEvent e){
-        Player p = e.getPlayer();
-        p.setBedSpawnLocation(SpawnPoint, true);
-        p.spawnParticle(Particle.CLOUD, p.getLocation(), 320);
-        if(!(p.hasPlayedBefore())) {
-            p.teleport(SpawnPoint);
-            p.playSound(p.getLocation(), Sound.BLOCK_PORTAL_TRAVEL, 0.2f, 1.0f);
-            p.sendTitle(ChatColor.RED + "The more you have, the worse it is",
+        Player player = e.getPlayer();
+        Location playerSpawnPoint = dungeonWorld.getPlayerSpawnPoint();
+        player.setBedSpawnLocation(playerSpawnPoint, true);
+        player.spawnParticle(Particle.CLOUD, player.getLocation(), 320);
+        if(!(player.hasPlayedBefore())) {
+            player.teleport(playerSpawnPoint);
+            player.playSound(player.getLocation(), Sound.BLOCK_PORTAL_TRAVEL, 0.2f, 1.0f);
+            player.sendTitle(ChatColor.RED + "The more you have, the worse it is",
                 ChatColor.YELLOW + "Plugin by HiWhatName & Earomc", 7, 160, 7);
         }
         //TODO: Change spawn location to start room, start nbs playback.
