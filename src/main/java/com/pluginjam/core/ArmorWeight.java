e54e7012da6c42a4e792ac502eb9b3757a3a05f9
HiWhatName <furo@localhost>
2022-11-28 22:20:04 +0100

    Bored,so fixed armor weight not working for other types.

--- src/main/java/com/pluginjam/core/ArmorWeight.java
+++ src/main/java/com/pluginjam/core/ArmorWeight.java
@@ -7,9 +7,11 @@ import static org.bukkit.Material.DEEPSLATE_EMERALD_ORE;
 
 public enum ArmorWeight {
     LEATHER( 0.0025f),
+    ELYTRA( 0.005f),
+    CHAIN(0.008f),
     IRON(0.011f),
-    GOLD(0.015f),
     DIAMOND( 0.013f),
+    GOLD(0.015f),
     NETHERITE(0.02f);
 
     private final float weight;
