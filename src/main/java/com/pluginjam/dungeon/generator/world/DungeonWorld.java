a53f66781c895207fec80529a75662e7db52a14f
HiWhatName <furo@localhost>
2022-11-29 11:38:45 +0100

    Add Gamerules to DungeonWorld.java

--- src/main/java/com/pluginjam/dungeon/generator/world/DungeonWorld.java
+++ src/main/java/com/pluginjam/dungeon/generator/world/DungeonWorld.java
@@ -2,23 +2,41 @@ package com.pluginjam.dungeon.generator.world;
 
 
 import org.bukkit.*;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityExplodeEvent;
-import org.bukkit.generator.ChunkGenerator;
-
-public class DungeonWorld extends ChunkGenerator {
+public class DungeonWorld{
     World Dungeon;
-    Location SpawnPoint = new Location(Dungeon, 0,75,0);
+
+    Location SpawnPoint;
     public DungeonWorld(String WorldName) {
         WorldCreator wc = new WorldCreator(WorldName);
-        wc.generator(new DungeonChunkGenerator()); //The chunk generator from step 1
+        wc.generator(new DungeonChunkGenerator());
         Dungeon = wc.createWorld();
 
-        //Dungeon.setStorm(false);
-        //Dungeon.setDifficulty(Difficulty.NORMAL);
+        this.SpawnPoint = new Location(Dungeon, 0,75,0); // Relative to Spawn-room
 
-        //TODO: FIX
+        Dungeon.setDifficulty(Difficulty.NORMAL);
+        Dungeon.setStorm(false);
+        Dungeon.setPVP(false);
+
+        Dungeon.setGameRule(GameRule.DO_FIRE_TICK, false);
+        Dungeon.setGameRule(GameRule.DO_TRADER_SPAWNING, false); // Well... I don't really want him here, do I?
+        Dungeon.setGameRule(GameRule.DO_PATROL_SPAWNING, false); // TODO: Maybe implement this as a (5 * dangerLevel)% chance on a new Piece spawn. // (Raid or something)
+        Dungeon.setGameRule(GameRule.DO_MOB_SPAWNING, false);
+        Dungeon.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false);
+        Dungeon.setGameRule(GameRule.DO_WEATHER_CYCLE, false);
+
+        Dungeon.setGameRule(GameRule.KEEP_INVENTORY, true); //IDK if we'll keep this tho.
+
+    }
+
+    public void init(){
+
+
+    }
+    public World getDungeonWorld(){
+        return Dungeon;
+    }
+    public Location getSpawnPoint(){
+        return this.SpawnPoint;
     }
 
 
