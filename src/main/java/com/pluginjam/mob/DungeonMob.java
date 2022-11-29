a4783c6d8b4c987c98d9fd2649158e52b57eff14
HiWhatName <furo@localhost>
2022-11-29 11:42:01 +0100

    Refactor DungeonMobs

--- src/main/java/com/pluginjam/mob/DungeonMob.java
+++ src/main/java/com/pluginjam/mob/DungeonMob.java
@@ -6,6 +6,8 @@ import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.World;
 import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
+import org.bukkit.entity.Creeper;
 import org.bukkit.entity.Mob;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -24,45 +26,52 @@ public abstract class DungeonMob<T extends Mob> {
      * true: uses the default mob name,
      * false: makes it possible to set own name
      */
-    private final boolean useDefaultName;
-
+    private final boolean miniBoss;
     @NotNull
     protected Class<T> baseEntityClass;
 
-    public DungeonMob(int level, @NotNull Class<T> baseEntityClass, boolean useDefaultName) {
+    public DungeonMob(int level, @NotNull Class<T> baseEntityClass, boolean miniBoss) {
         this.level = level;
         this.baseEntityClass = baseEntityClass;
         this.entity = null;
-        this.useDefaultName = useDefaultName;
+        this.miniBoss = miniBoss;
     }
+    
     public DungeonMob(int level, @NotNull Class<T> baseEntityClass) {
-        this(level, baseEntityClass, true);
+        this(level, baseEntityClass, false);
     }
 
     public T spawn(SpawnMeta spawnMeta) {
         World world = spawnMeta.world();
         Location location = spawnMeta.location();
-        T spawned = world.spawn(location, baseEntityClass);
-        if (useDefaultName) {
-            spawned.setCustomName(Util.generateMobName(this));
-            spawned.setCustomNameVisible(true);
-        }
-        // Set stats depended on level
-
-        spawned.addPotionEffect(new PotionEffect(PotionEffectType.HEALTH_BOOST, 99999, Math.round(level / 4), false, false)); //we cant use .setHealth()
-        spawned.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE).setBaseValue(0.3f + spawned.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE).getBaseValue() * level / 4.5);
         Random rnd = new Random();
-        int i = rnd.nextInt(65);
-        if(i <= level){
-            spawned.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE).setBaseValue(0.3f + spawned.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE).getBaseValue() * level / 3.75);
-            spawned.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, 99999, 1, false, false));
-            spawned.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 9999, 1, false, false));
-            spawned.setCustomName(ChatColor.RED + Util.generateMobName(this));
+        T spawned = world.spawn(location, baseEntityClass);
 
+        if(miniBoss){
+            spawned.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, Integer.MAX_VALUE, 1, false, false));
+            spawned.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, Integer.MAX_VALUE, 1, false, false)); // + 130% Dmg
+            spawned.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(spawned.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).getBaseValue() * 1.15f); // 15% Faster
+            spawned.getAttribute(Attribute.GENERIC_ARMOR).setBaseValue(spawned.getAttribute(Attribute.GENERIC_ARMOR).getBaseValue() * 1.25f); // 25% More armor since we won't add more hp.
+            spawned.setCustomName(ChatColor.RED + Util.generateMobName(this, true, rnd));
+        }else{
+            spawned.setCustomName(Util.generateMobName(this));
+        }
+
+        //AttributeInstance dmgAttribute = Util.get.... After 5hp dmg, we'll only add 1/2
+        double baseDMG = spawned.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE).getBaseValue();
+        if(baseDMG * level / 4 > baseDMG + 5f){ spawned.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE).setBaseValue(5f + ((baseDMG * level / 3.5) - 5f) / 2);
+        }else{ spawned.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE).setBaseValue(baseDMG * level / 3.5); } //TODO: Change this
+
+        Util.setHealth(spawned, spawned.getHealth() + (level / 1.75));
+
+        if(spawned instanceof Creeper creeper){
+            creeper.setExplosionRadius((creeper.getExplosionRadius() + Math.round(level / 8.5f))); // Lvl. 20: +2.3 radius
+            creeper.setMaxFuseTicks(creeper.getMaxFuseTicks() - level); //Lvl. 10: -0.5sec
         }
 
         return spawned;
     }
+
 
     public abstract String getDisplayName();
 
--- src/main/java/com/pluginjam/mob/DungeonMobManager.java
+++ src/main/java/com/pluginjam/mob/DungeonMobManager.java
@@ -6,11 +6,9 @@ import org.bukkit.event.entity.EntityDeathEvent;
 
 public class DungeonMobManager {
 
-    //RandomCollection<String> rc = new RandomCollection<>().add(13,new ItemBuilder(Material.LEATHER_CHESTPLATE).setName("Used miner's chest-plate").build()).add(
-           // 16,new ItemBuilder(Material.LEATHER_LEGGINGS).setName("Used miner's leggings").build()).add(15, new ItemBuilder(Material.LEATHER_BOOTS).setName("Used miner's boots").build()).add(
-                  //  15, new ItemBuilder(Material.LEATHER_BOOTS).setName("Used miner's boots").build()).add(8, new ItemBuilder(Material.LEATHER_HELMET).setName("Used miner's helmet").build()).add(63, new ItemStack().ROTTEN_FLESH);
+    private void applyStatBoosts(DungeonMob dungeonMob, boolean special){
 
-
+    }
     @EventHandler(priority = EventPriority.HIGH)
     public void onEntityDeath(EntityDeathEvent e){
         if(e.getEntity() instanceof DungeonMob<?>){
